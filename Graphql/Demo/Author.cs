88da1c1c6e122276aa17259af602dfbfeb8670d5
WonderI3iscuit <58702197+WonderI3iscuit@users.noreply.github.com>
2021-04-20 07:14:57 -0700

    Add files via upload

---
+++ Graphql/Demo/Author.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class Query
+    {
+        public Book GetBook() =>
+            new Book
+            {
+                Title = "C# in depth.",
+                Author = new Author
+                {
+                    Name = "Jon Skeet"
+                }
+            };
+    }
+}
+
